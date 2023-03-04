3d3f7f289c227ecffee24ac6a7638399bb865825
neanie1 <neavnieminen@gmail.com>
2023-03-04 10:32:07 +0200

    commit

---
+++ db.sql
@@ -0,0 +1,16 @@
+drop database if exist shoppinglist;
+
+create database shoppinglist;
+
+use shoppinglist;
+
+create table item (
+    id int primary key auto_increment,
+    description varchar(255) not null, 
+    amount smallint unsigned not null
+);
+
+insert into item (description, amount) values ('Test item', 1);
+insert into item (description, amount) values ('Another test item', 2);
+insert into item (description, amount) values ('One more test item', 3);
+
